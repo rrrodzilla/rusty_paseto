550e94049bd30ad86597ab5e3180454ff114d9e8
rodzilla <rrrodzilla@protonmail.com>
2022-03-05 23:21:19 -0800

    Add actix_identity example

---
+++ examples/actix_identity/main.rs
@@ -0,0 +1,125 @@
+/// This example shows how you might use a PASETO token to store some data in a cookie once the
+/// user has been logged in.  
+///
+/// run the following command from your shell to:
+/// 1) visit the site as an anonymous user and then,
+/// 2) login the user, creating a paseto token and storing it in a cookie using the identity as the
+///    implicit assertion and then,
+/// 3) logout the user, forgetting the identity
+/// 4) attempting to visit a secure path as a logged out user (will panic)
+///
+/// curl http://localhost:8080;curl -X POST http://localhost:8080/login -c ~/cookies; curl
+/// http://localhost:8080/app/secure -b ~/cookies; curl -X POST http://localhost:8080/logout -b
+/// ~/cookies
+///
+use rusty_paseto::prelude::*;
+use actix_web::http::StatusCode;
+use actix_web::cookie::{Cookie, SameSite};
+use actix_web::web;
+use actix_web::{post,get, HttpResponse, HttpServer, App, services};
+use actix_identity::{Identity, CookieIdentityPolicy, IdentityService};
+use time::OffsetDateTime;
+use uuid::Uuid;
+
+mod paseto;
+use paseto::PasetoCookieIdentityPolicy;
+
+#[get("/secure")]
+async fn secure(id: Identity) -> String {
+    // access request identity
+    if let Some(id) = id.identity() {
+        format!("Logged in Secure User: {}\n", id)
+    } else {
+        "Welcome Anonymous!".to_owned()
+    }
+}
+
+#[get("/")]
+async fn index(id: Identity) -> String {
+    // access request identity
+    if let Some(id) = id.identity() {
+        format!("Welcome! {}", id)
+    } else {
+        println!("Found new anonymous user\n");
+        "Welcome Anonymous!\n".to_owned()
+    }
+}
+
+#[post("/login")]
+async fn login(id: Identity, data: web::Data<AppData>) -> HttpResponse {
+    // here you might do whatever checks are needed to authenticate user
+
+    // create a new identity and wrap it in an auth cookie
+    let authenticated_user_id : &'static str = Box::leak(Uuid::new_v4().to_string().into_boxed_str());
+    println!("Logged in user {}\n", authenticated_user_id);
+
+    //create an implicit assertion (or you could create claim)
+    let assertion = ImplicitAssertion::from(authenticated_user_id);
+
+    //creating a paseto key
+          let key_val = data.paseto_key.as_bytes();
+      let key = PasetoSymmetricKey::<V4, Local>::from(Key::from(key_val));
+
+      //create a token using the identity as an implicit assertion (you could also use a claim if
+      //needed)
+let token = PasetoBuilder::<V4, Local>::default()
+        .set_implicit_assertion(assertion)
+      .build(&key)
+      .expect("Couldn't create paseto auth token");
+
+    // remember new authenticated identity
+    id.remember(authenticated_user_id.to_string());
+
+    // return the response creating a new cookie to hold the token
+     HttpResponse::build(StatusCode::OK).cookie(Cookie::build("auth-token", token).path("/").expires(OffsetDateTime::now_utc()).secure(false).http_only(true).same_site(SameSite::Lax).finish()).finish()
+
+ 
+}
+
+#[post("/logout")]
+async fn logout(id: Identity) -> String {
+    println!("Logging out user {}\n", id.identity().expect("Couldn't get identity"));
+    //build logout msg
+       let logout_msg = format!("Goodbye {}!\n", id.identity().expect("Couldn't get identity")).to_owned();
+    // remove identity
+    id.forget();
+    logout_msg
+    
+}
+
+//shared state
+pub(crate) struct AppData {
+    paseto_key: &'static str
+}
+
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+ HttpServer::new(move || {
+    // create cookie identity backend (inside closure, since policy is not Clone)
+    let policy = IdentityService::new(CookieIdentityPolicy::new(&[0; 32])
+        .name("auth-cookie")
+        .secure(false));
+
+    // create a paseto cookie policy, for this use case, however I recommend using a middleware but
+     // this shows how you might use a policy instead
+     let paseto_policy = PasetoCookieIdentityPolicy {};
+
+     //paths that are not verified with the paseto token
+     let unauthenticated_scope = web::scope("").service(services![index, login, logout]);
+     //paths that should verify that a token exists and is valid
+     let authenticated_scope = web::scope("/app").wrap(IdentityService::new(paseto_policy)).service(services![secure]);
+
+     //create and run the server
+    App::new()
+        .app_data(web::Data::new(AppData { paseto_key:  "wubbalubbadubdubwubbalubbadubdub"}))
+        .wrap(policy)
+        // wrap policy into middleware identity middleware
+        .service(authenticated_scope)
+        .service(unauthenticated_scope)
+})     .bind(("127.0.0.1", 8080))?
+    .run()
+    .await  
+}
+
+
---
+++ examples/actix_identity/paseto.rs
@@ -0,0 +1,58 @@
+use crate::AppData;
+use actix_identity::{IdentityPolicy, RequestIdentity};
+use actix_utils::future::{ready, Ready};
+use actix_web::{
+  dev::{ServiceRequest, ServiceResponse},
+  error::{Error, Result},
+  web::Data,
+};
+use rusty_paseto::prelude::*;
+pub struct PasetoCookieIdentityPolicy {}
+
+fn validate_auth_token(request: &mut ServiceRequest) -> Result<Option<String>, Error> {
+  //try to find the cookie with the auth token, panic if not found
+  //ideally we should map the errors to an http not authorized error
+  let cookie = request
+    .cookie("auth-token")
+    .expect("No auth token found in PasetoCookieIdentityPolicy");
+  //now grab the token from the cookie
+  let token: &str = cookie.value();
+
+  //get the identity from the identity cookie
+  let identity = request.get_identity().expect("Couldn't find identity");
+  let id: &'static str = Box::leak(identity.clone().into_boxed_str());
+  //get the paseto key from the shared state
+  let key_val = request.app_data::<Data<AppData>>().unwrap().paseto_key.as_bytes();
+  //create a paseto key
+  let key = PasetoSymmetricKey::<V4, Local>::from(Key::from(key_val));
+
+  //attempt to parse the token when accessing a secure path, in practice this should also map to an HTTP error
+  PasetoParser::<V4, Local>::default()
+    .set_implicit_assertion(ImplicitAssertion::from(id))
+    .parse(token, &key)
+    .map_err(|err_val| println!("{}", err_val))
+    .expect("Couldn't validate authentication token");
+  println!(
+    "Validated auth token in PasetoCookieIdentityPolicy\n  for user {}\n",
+    id
+  );
+  Ok(Some(identity))
+}
+
+impl IdentityPolicy for PasetoCookieIdentityPolicy {
+  type Future = Ready<Result<Option<String>, Error>>;
+  type ResponseFuture = Ready<Result<(), Error>>;
+
+  fn from_request(&self, request: &mut ServiceRequest) -> Self::Future {
+    ready(validate_auth_token(request))
+  }
+
+  fn to_response<B>(
+    &self,
+    _identity: Option<String>,
+    _changed: bool,
+    _response: &mut ServiceResponse<B>,
+  ) -> Self::ResponseFuture {
+    ready(Ok(()))
+  }
+}

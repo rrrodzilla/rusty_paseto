5e919b0afa109e3da671169ff51d2089be9e630b
rrrodzilla <roland@govcraft.ai>
2024-07-22 15:48:22 -0600

    Revert "chore: update aes to 0.8 (#44)"
    This reverts commit 7ba2ae880cd1425ca1326ab6c3699f432145d4c7.

--- src/core/common/cipher_text_impl/v1_local.rs
+++ src/core/common/cipher_text_impl/v1_local.rs
@@ -1,12 +1,11 @@
 #![cfg(feature = "v1_local")]
 use std::marker::PhantomData;
+use aes::Aes256Ctr;
 use aes::cipher::generic_array::GenericArray;
-use aes::cipher::{KeyIvInit, StreamCipher};
+use aes::cipher::{NewCipher, StreamCipher};
 use crate::core::common::cipher_text::CipherText;
 use crate::core::{Local, V1};
 use crate::core::common::EncryptionKey;
-
-type Aes256Ctr = ctr::Ctr64BE<aes::Aes256>;
 
 impl CipherText<V1, Local> {
     pub(crate) fn from(payload: &[u8], encryption_key: &EncryptionKey<V1, Local>) -> Self {

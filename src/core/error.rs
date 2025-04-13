8979ca271e34f9b0b6c9ddd87177ad72be534a92
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-04-14 01:40:24 +0300

    resolve conflicting implementations when the From<ed25519_dalek::ed25519::Error> is implemented

--- src/core/error.rs
+++ src/core/error.rs
@@ -35,7 +35,6 @@ pub enum PasetoError {
   #[error("An unspecified cipher error occurred")]
   RsaCipher {
     ///An RSA cipher error
-    #[from]
     source: ed25519_dalek::ed25519::Error,
   },
   #[cfg(feature = "p384")]
@@ -103,4 +102,11 @@ pub enum PasetoError {
     #[from]
     source: std::string::FromUtf8Error,
   },
+}
+
+#[cfg(all(feature = "ed25519-dalek", not(feature = "p384")))]
+impl From<ed25519_dalek::ed25519::Error> for PasetoError {
+  fn from(source: ed25519_dalek::ed25519::Error) -> Self {
+      PasetoError::RsaCipher{source}
+  }
 }
